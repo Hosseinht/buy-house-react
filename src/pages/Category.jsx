2784340646765a51c277738544b644df66622a64
Ryan <sailantht@gmail.com>
2022-04-22 10:44:59 +0430

    Fetch Listings From Firestore

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,8 +1,83 @@
+import {useEffect, useState} from "react";
+// useEffect: make the first call to get our listing from firebase
+import {useParams} from "react-router-dom";
+import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'firebase/firestore'
+import {db} from "../firebase.config";
+import {toast} from "react-toastify";
+import Spinner from "../components/Spinner";
+
 const Category = () => {
-    // here we want
+    // here we want to fetch the listing from firebase
+    const [listings, setListings] = useState(null)
+    const [loading, setLoading] = useState()
+
+    const params = useParams()
+
+    useEffect(() => {
+        // since we are going to be using async await and, we can't do on useEffect like
+        // this useEffect( async () => {} we have to create a function
+        const fetchListings = async () => {
+            try {
+                // Get a reference
+                const listingsRef = collection(db, 'listings')
+
+                // Create a query
+                const q = query(
+                    listingsRef,
+                    where('type', '==', params.categoryName),
+                    orderBy('timestamp', 'desc'),
+                    limit(10)
+                )
+                // categoryName? url in the App.js
+                // what is type in where? it's our database. in our database there is field name type
+                // which is rent or sell
+
+                // Execute query
+                const querySnap = await getDocs(q)
+
+                const listings = []
+
+                querySnap.forEach((doc) => {
+                    return listings.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                    // doc.data() is going to give us the data but the ID, the document ID, that's going to
+                    // be in this doc.id. it's separate from data. in data there is no ID
+                })
+                setListings(listings)
+                setLoading(false)
+
+
+            } catch (error) {
+                toast.error('Could not fetch listings')
+            }
+        }
+        fetchListings()
+    }, [])
+
     return (
-        <div>
-            Category
+        <div className='category'>
+            <header className='pageHeader'>
+                {params.categoryName === 'rent' ? 'Places for rent' : 'Places for sell'}
+            </header>
+
+            {loading ? (
+                    <Spinner/>)
+                : listings && listings.length > 0 ? (
+                    <>
+                        <main>
+                            <ul className="categoryListings">
+                                {listings.map((listing) => (
+                                    <h3 key={listing.id}>{listing.data.name}</h3>
+                                ))}
+                            </ul>
+                        </main>
+                    </>
+                ) : (
+                    <p>No listings for {params.categoryName}</p>
+                )
+            }
         </div>
     );
 };
